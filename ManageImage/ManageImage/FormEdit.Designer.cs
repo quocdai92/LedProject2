26d3b802c557882104715cc5164f1c00dffa8722
Phạm Tiến Đạt <datpt6563@seta-international.vn>
2016-07-30 20:23:23 +0700

    Play file

--- ManageImage/ManageImage/FormEdit.Designer.cs
+++ ManageImage/ManageImage/FormEdit.Designer.cs
@@ -80,6 +80,7 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(421, 370);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
             // 
             // panel2
             // 
